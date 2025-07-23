328016522bc419b7b8b18c7a4cbe228228534a8c
Luis <luisitocarret@gmail.com>
2025-07-23 16:43:33 -0600

    cambios nuevos chido 2

--- firebase.js
+++ firebase.js
@@ -1,8 +1,10 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebase/serviceAccountKey.json";
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
-   credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();

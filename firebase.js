cee3ac68d2d914ef317d57574d1306cbcc50fb19
Luis <luisitocarret@gmail.com>
2025-07-23 13:45:35 -0600

    cambios nuevos chido

--- firebase.js
+++ firebase.js
@@ -1,12 +1,5 @@
 import admin from "firebase-admin";
-
-const firebaseConfig = process.env.FIREBASE_CONFIG;
-
-// ✅ Agrega este log para ver si llegó correctamente
-console.log("📦 FIREBASE_CONFIG:", firebaseConfig);
-
-const serviceAccount = JSON.parse(firebaseConfig);
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+import serviceAccount from "./firebase/serviceAccountKey.json";
 
 admin.initializeApp({
    credential: admin.credential.cert(serviceAccount),

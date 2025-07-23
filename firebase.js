b180f6e238f69dafe7130d5e4e3fe2e870843d1a
Luis <luisitocarret@gmail.com>
2025-07-23 12:40:34 -0600

    cambios nuevos 2

--- firebase.js
+++ firebase.js
@@ -1,12 +1,11 @@
 import admin from "firebase-admin";
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
-if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
-}
 
 const db = admin.firestore();
 export { db };
